992b034d23a958a38d1241a89ad4a516e6ee67c5
zackteo <zackteo@gmail.com>
2020-12-05 22:59:03 +0800

    Change send script and run async

--- python_scripts/parse-json.py
+++ python_scripts/parse-json.py
@@ -24,22 +24,6 @@ for i in data["Stacks"][0]["Outputs"]:
 dns = outputs["Instance1PublicIP"]
 
 # send aws-key to namenode
-
-print(dns)
-
-command = (
-    "scp -i ~/.ssh/"
-    + ssh_key
-    + " -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-    + " ~/.ssh/"
-    + ssh_key
-    + " ubuntu@"
-    + dns
-    + ":home/ubuntu/.ssh/id_rsa"
-)
-
-print(command)
-
 os.system(
     "scp -i ~/.ssh/"
     + ssh_key
@@ -51,8 +35,35 @@ os.system(
     + ":/home/ubuntu/.ssh/id_rsa"
 )
 
-# create /etc/hosts for all
+# send scripts to nodes
+for ip in outputs.keys():
+    os.system(
+        "scp -i ~/.ssh/"
+        + ssh_key
+        + " -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+        + " ~/book-review-site-automation/analytics_setup/datanode.sh"
+        + " ubuntu@"
+        + ip
+        + ":/home/ubuntu/setup.sh"
+    )
 
+os.system(
+    "scp -i ~/.ssh/"
+    + ssh_key
+    + " -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+    + " ~/book-review-site-automation/analytics_setup/namenode.sh"
+    + " ubuntu@"
+    + dns
+    + ":/home/ubuntu/setup.sh"
+)
+
+# run scripts
+for ip in outputs.keys():
+    os.system("ssh ubuntu@" + ip + " -i ~/.ssh/" + ssh_key + "chmod +x setup.sh")
+    os.system("ssh ubuntu@" + ip + " -i ~/.ssh/" + ssh_key + "./setup.sh &")
+
+
+# create /etc/hosts for all
 
 host_file = """
 # /etc/hosts
